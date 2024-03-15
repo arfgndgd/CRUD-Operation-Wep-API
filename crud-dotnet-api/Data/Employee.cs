f5ad12bbc9f33384958860fa10500bbaee91b5b8
arfgndgd <gundogduarif4@gmail.com>
2024-03-15 05:57:05 +0300

    Added some classes

---
+++ crud-dotnet-api/Data/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace crud_dotnet_api.Data
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int Age { get; set; }
+        public int Salary { get; set; }
+    }
+}
