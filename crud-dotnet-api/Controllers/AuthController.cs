3d6e72d9ea060abbb3eaeb822dab96aa5d01a74d
arfgndgd <gundogduarif4@gmail.com>
2024-03-28 16:23:00 +0300

    logindto, authcontroller added

---
+++ crud-dotnet-api/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+﻿using crud_dotnet_api.Data;
+using crud_dotnet_api.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace crud_dotnet_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly EmployeeRepository _employeeRepository;
+
+        public AuthController(EmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login([FromBody] LoginDto model)
+        {
+            //validate email and password
+            var employee = await _employeeRepository.GetEmployeeByEmail(model.Email); 
+            if (employee == null)
+            {
+                return BadRequest(new { error = "email does not exist" });
+            }
+            if (employee.Password != model.Password)
+            {
+                return BadRequest(new { error = "email/password is incorrect" });
+            }
+            var token = "";
+            return Ok(token);
+        }
+    }
+}
